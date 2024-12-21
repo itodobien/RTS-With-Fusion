3860d33f8fd7fa90d839fcba297b6debaf2498d9
itodobien <itodobien@gmail.com>
2024-12-20 18:16:07 -0800

    Small Clean Up
    Made an enum and method to handle mouse state.

--- Assets/Scripts/Enums/SelectionModeEnum.cs
+++
@@ -1,6 +0,0 @@
-public enum SelectionMode 
-{
-    Default,    // Normal selection, clears previous selection
-    Additive,   // Adds to current selection (Shift key)
-    Subtractive // Removes from current selection (Ctrl key)
-}
